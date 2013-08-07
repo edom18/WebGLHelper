17ce24107287105dd30b6c6634fb2265cb4c1808
edom18 <edo.m18@gmail.com>
2013-08-07 16:57:46 +0900

    renamed the method `addVectors` to `add`.

--- js/lib/vec4.js
+++ js/lib/vec4.js
@@ -78,7 +78,7 @@
         return deset;
     };
 
-    vec4.addVectors = function(v1, v2, dest) {
+    vec4.add = function(v1, v2, dest) {
         dest[0] = v1[0] + v2[0];
         dest[1] = v1[1] + v2[1];
         dest[2] = v1[2] + v2[2];
