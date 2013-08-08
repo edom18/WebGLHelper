25883a4e1c865aa032cb9aabb51b4d3c742e04a4
edom18 <edo.m18@gmail.com>
2013-08-08 10:02:41 +0900

    update demo.

--- js/main.js
+++ js/main.js
@@ -13,11 +13,10 @@
     $gl.setViewport(0, 0, w, h);
     $gl.setClearColor(0.0, 0.0, 0.0, 1.0);
 
-    var v_shader = $gl.getShaderSourceFromDOM('vs');
-    var f_shader = $gl.getShaderSourceFromDOM('fs');
-    var vs = $gl.createShader('vertex', v_shader);
-    var fs = $gl.createShader('fragment', f_shader);
-    var prg = $gl.createProgram(vs, fs);
+    var prg = $gl.createProgramWithShader({
+        vertexShader:   $gl.getShaderSourceFromDOM('vs'),
+        fragmentShader: $gl.getShaderSourceFromDOM('fs')
+    });
 
     var position = [
         -1.0,  1.0, 0.0, //v0
@@ -37,6 +36,12 @@
         1.0, 1.0, //v2
         1.0, 0.0  //v0
     ];
+    var normals = [
+        0.0, 0.0, 1.0, //v0
+        0.0, 0.0, 1.0, //v1
+        0.0, 0.0, 1.0, //v2
+        0.0, 0.0, 1.0  //v3
+    ];
 
     var indecies = [
         0, 1, 2, 0, 2, 3
@@ -46,42 +51,53 @@
     var ibo = $gl.createBuffer('ibo', indecies);
     var color_vbo = $gl.createBuffer('vbo', colors);
     var tex_coord_vbo = $gl.createBuffer('vbo', tex_coords);
+    var normal_vbo = $gl.createBuffer('vbo', normals);
 
     var texture = $gl.createTexture('img/logo.jpg');
 
     var attLoc = gl.getAttribLocation(prg, 'a_position');
     var attLoc2 = gl.getAttribLocation(prg, 'a_color');
     var attLoc3 = gl.getAttribLocation(prg, 'a_texCoord');
+    var attLoc4 = gl.getAttribLocation(prg, 'a_normal');
 
-    var uniLoc = gl.getUniformLocation(prg, 'u_mvpMatrix');
+    var uniLoc = gl.getUniformLocation(prg, 'u_mvMatrix');
     var uniLoc2 = gl.getUniformLocation(prg, 'u_texture');
+    var uniLoc3 = gl.getUniformLocation(prg, 'u_lightPosition');
+    var uniLoc4 = gl.getUniformLocation(prg, 'u_pMatrix');
+    var uniLoc5 = gl.getUniformLocation(prg, 'u_NMatrix');
 
     gl.enableVertexAttribArray(attLoc);
     gl.enableVertexAttribArray(attLoc2);
     gl.enableVertexAttribArray(attLoc3);
+    gl.enableVertexAttribArray(attLoc4);
 
     var angle = 0;
     var z = 10;
+    var light_position = vec3(100, 0, 0);
 
     //プロジェクション変換マトリクスの生成
-    var projMatrix = mat4.perspective(60, w / h, 1, 100, mat4());
+    var projMatrix = mat4.perspective(60, w / h, 1, 100);
 
     (function loop() {
 
         //モデル変換マトリクスを生成
         var modelMatrix = mat4();
 
-        //最終的に使用されるMVP用マトリクスを生成
-        var mvpMatrix   = mat4();
+        //モデルビュー行列
+        var mvMatrix = mat4();
+        
+        //法線用マトリクス
+        var nMatrix = mat4();
 
         //ビュー座標変換マトリクスの生成
-        var viewMatrix = mat4.lookAt(vec3(0, 0, z), vec3(0, 0, 0), vec3(0, 1, 0), mat4());
+        var viewMatrix = mat4.lookAt(vec3(0, 0, z), vec3(0, 0, 0), vec3(0, 1, 0));
 
         angle = (angle + 1) % 360;
         mat4.rotate(modelMatrix, angle, vec3(0, 1, 0), modelMatrix);
         mat4.scale(modelMatrix, vec3(5, 5, 5), modelMatrix);
-        mat4.multiply(projMatrix, viewMatrix, mvpMatrix);
-        mat4.multiply(mvpMatrix, modelMatrix, mvpMatrix);
+        mat4.multiply(viewMatrix, modelMatrix, mvMatrix);
+        mat4.copy(mvMatrix, nMatrix);
+        mat4.transpose(mat4.inverse(nMatrix, nMatrix));
 
         /*! ----------------------------------------------------------------------------------
          * draw**を呼び出す前に、そのdrawメソッドで使用するバッファ、
@@ -100,6 +116,10 @@
         gl.bindBuffer(gl.ARRAY_BUFFER, tex_coord_vbo);
         gl.vertexAttribPointer(attLoc3, 2, gl.FLOAT, false, 0, 0);
 
+        //頂点の法線情報をバインド
+        gl.bindBuffer(gl.ARRAY_BUFFER, normal_vbo);
+        gl.vertexAttribPointer(attLoc4, 3, gl.FLOAT, false, 0, 0);
+
         //インデックスバッファをバインド
         //gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, ibo);
 
@@ -108,8 +128,17 @@
         gl.bindTexture(gl.TEXTURE_2D, texture);
         gl.uniform1i(uniLoc2, texture);
 
-        //最終的なMVPマトリクスをアップロード
-        gl.uniformMatrix4fv(uniLoc, false, mvpMatrix);
+        //プロジェクションマトリクスをアップロード
+        gl.uniformMatrix4fv(uniLoc4, false, projMatrix);
+
+        //モデルビューマトリクスをアップロード
+        gl.uniformMatrix4fv(uniLoc, false, mvMatrix);
+
+        //法線用マトリクスをアップロード
+        gl.uniformMatrix4fv(uniLoc5, false, nMatrix);
+
+        //ライトの位置をアップロード
+        gl.uniform3fv(uniLoc3, light_position);
 
         //色をクリア
         gl.clear(gl.COLOR_BUFFER_BIT);
