8995a051a66c367367caffab3aecf8addb311b26
edom18 <edo.m18@gmail.com>
2014-10-13 01:53:11 +0900

    added a few methods.

--- js/lib/WebGLHelper.js
+++ js/lib/WebGLHelper.js
@@ -317,6 +317,8 @@
             //テクスチャパラメータ
             gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
             gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
 
             //フレームバッファにテクスチャをアタッチ
             gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, texture, 0);
